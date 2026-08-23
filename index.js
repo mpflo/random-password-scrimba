76ef7b3846a916d1f3f5c48d1968b49aeb86abbe
mpflo <qipeot@gmail.com>
2026-08-23 17:26:31 -0300

    distribui 'banco' de senhas em três linhas e, de acordo com a escolha do user, cria um novo 'banco' na array characters

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let generatePassword = document.getElementById("generate-password")
+let passwordLength = 15
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
+let numbersOption = document.getElementById("numbers")
+let symbolsOption = document.getElementById("symbols")
+
 const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -5,12 +12,7 @@ const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"]
 
-const characters = []
-
-let generatePassword = document.getElementById("generate-password")
-let passwordLength = 15
-let passwordOne = document.getElementById("password-one")
-let passwordTwo = document.getElementById("password-two")
+let characters = []
 
 function choosingPassword() {
     let password = ""
@@ -23,7 +25,19 @@ function choosingPassword() {
     return password
 }
 
-function generatePasswords() {   
+function generatePasswords() {       
+    characters = []
+    
+    if (!numbersOption.checked && !symbolsOption.checked) {
+        characters.push(...letters)
+    } else if (!numbersOption.checked && symbolsOption.checked) {
+        characters.push(...symbols, ...letters)
+    } else if (numbersOption.checked && !symbolsOption.checked) {
+        characters.push(...numbers, ...letters)
+    } else {
+        characters.push(...letters, ...numbers, ...symbols)
+    }
+
     passwordOne.value = choosingPassword()
     passwordTwo.value = choosingPassword()
 }

c268f505fabbc60b7367d9622d8643304c7612db
mpflo <qipeot@gmail.com>
2026-08-26 22:31:58 -0300

    organiza código

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+
+let characters = []
+
 let generatePassword = document.getElementById("generate-password")
 let passwordOne = document.getElementById("password-one")
 let passwordTwo = document.getElementById("password-two")
@@ -28,18 +37,6 @@ inputsPassword.forEach(function(inputPassword) {
 
     
 })
-
-
-
-
-const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"]
-
-let characters = []
 
 function choosingPassword() {
     let password = ""
